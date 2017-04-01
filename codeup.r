6a4d499766aba972fd76e995b57ccc0ae78999e2
karan jagota <karanjagota@users.noreply.github.com>
2017-04-01 21:40:34 +0530

    Update codeup.r

--- codeup.r
+++ codeup.r
@@ -1,5 +1,6 @@
 library(tm)
 library(wordcloud)
+library(RSentiment)
 
 a1<- read.csv("Election_tweets.csv")
 
@@ -14,6 +15,10 @@ a1$TweetText<- gsub("[!]", " ", a1$TweetText)
 a1$TweetText<- gsub("[?]" , " " , a1$TweetText)
 a1$TweetText<- gsub("[,]" , " ", a1$TweetText)
 a1$TweetText<- gsub('http.*\\s*', ' ', a1$TweetText)
+
+# Inorder to remove unicode you can uncomment below code..  
+# a1$TweetText <- sapply(a1$TweetText,function(row) iconv(row, "latin1", "ASCII", sub=" "))
+
 
 id<- c(1:8356)
 b<- unique(a1$TweetText)
@@ -33,8 +38,17 @@ copy_document_term_matrix <- as.matrix(document_term_matrix)
 
 frequency <- colSums(copy_document_term_matrix)
 
+# Calculating sentiment score .. 
+sentiments_up = calculate_sentiment(names(frequency))
+sentiments_up = cbind(sentiments_up, as.data.frame(frequency))
+sent_pos_up = sentiments_up[sentiments_up$sentiment == 'Positive',]
+sent_neg_up = sentiments_up[sentiments_up$sentiment == 'Negative',]
+
 # making wordcloud .. 
 set.seed(415)
 wordcloud(words, frequency,max.words = 200,scale = c(10,0.5))
 
+# before making sentiment word cloud with sentiment score one has to remove emoji code from tweets which has been comented above.... 
+wordcloud(sent_pos_up$text,sent_pos_up$freq,scale =c(10,0.5))
 
+wordcloud(sent_neg_up$text,sent_neg_up$freq,scale =c(10,0.5) )
